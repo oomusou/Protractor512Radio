1cb4b43d228dd3674f75c7be43d1aaab34eb19e7
Oomusou <oomusou@gmail.com>
2017-08-26 20:59:04 +0800

    Acceptance Test (failed)

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -5,10 +5,32 @@ describe('protractor512-radio App', () => {
 
   beforeEach(() => {
     page = new AppPage();
+    page.navigateTo();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it(`should have '3' labels`, () => {
+    expect(page.getLabelCount()).toBe(3);
+  });
+
+  it(`should have '3' radios`, () => {
+    expect(page.getRadioCount()).toBe(3);
+  });
+
+  it(`should get '0' when select 'AWS'`, () => {
+    page.selectCloudByIndex(0);
+    expect(page.getSelectedCloud()).toBe('0');
+    expect(page.getSelectedId()).toBe('0');
+  });
+
+  it(`should get '1' when select 'Azure'`, () => {
+    page.selectCloudByText('Azure');
+    expect(page.getSelectedCloud()).toBe('1');
+    expect(page.getSelectedId()).toBe('1');
+  });
+
+  it(`should get '2' when select 'GCP'`, () => {
+    page.selectCloudByText('GCP');
+    expect(page.getSelectedCloud()).toBe('2');
+    expect(page.getSelectedId()).toBe('2');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -5,7 +5,33 @@ export class AppPage {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getLabelCount(): any {
+    return element.all(by.tagName('label')).count();
+  }
+
+  getRadioCount(): any {
+    return element.all(by.css('input[type="radio"]')).count();
+  }
+
+  selectCloudByIndex(index: number): AppPage {
+    element.all(by.tagName('label'))
+      .get(index)
+      .click();
+
+    return this;
+  }
+
+  selectCloudByText(cloud: string): AppPage {
+    element(by.cssContainingText('label', cloud)).click();
+
+    return this;
+  }
+
+  getSelectedCloud(): any {
+    return element(by.css('input[name="cloud"]:checked')).getAttribute('value');
+  }
+
+  getSelectedId(): any {
+    return element(by.css('p')).getText();
   }
 }
