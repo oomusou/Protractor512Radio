8fe7dce6f035a774a00c1b0a2b382b2909d8d390
Oomusou <oomusou@gmail.com>
2017-08-26 21:31:21 +0800

    Acceptance Test (passed)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cloud } from './cloud';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  selectedId = '0';
+  clouds: Cloud[] = [
+    {id: '0', name: 'AWS', checked: true},
+    {id: '1', name: 'Azure', checked: false},
+    {id: '2', name: 'GCP', checked: false}
+  ];
+
+  onChange(element: HTMLInputElement) {
+    this.selectedId = element.value;
+  }
 }
---
+++ src/app/cloud.ts
@@ -0,0 +1,5 @@
+export interface Cloud {
+  id: string;
+  name: string;
+  checked: boolean;
+}
